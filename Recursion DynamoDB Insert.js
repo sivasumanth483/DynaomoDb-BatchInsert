bda1e980c40d6c8b0df572a17dabda59817d9798
sivasumanth483 <38774340+sivasumanth483@users.noreply.github.com>
2019-08-24 10:22:43 +0530

    Add files via upload

---
+++ Recursion DynamoDB Insert.js
@@ -0,0 +1,315 @@
+/* jshint esversion:9 */
+
+data = [
+    {
+        "entity_id": "5d60bb9585d9e71833430c51"
+    },
+    {
+        "entity_id": "5d60bb95968c4045997b45d6"
+    },
+    {
+        "entity_id": "5d60bb95c6549fcc5d22fd52"
+    },
+    {
+        "entity_id": "5d60bb9575b3f801bf52553c"
+    },
+    {
+        "entity_id": "5d60bb95aa2f1e6ee7351ba1"
+    },
+    {
+        "entity_id": "5d60bb95b81e817aab2db887"
+    },
+    {
+        "entity_id": "5d60bb9583ba9baaf85ffb9f"
+    },
+    {
+        "entity_id": "5d60bb9565ec9f93c232334e"
+    },
+    {
+        "entity_id": "5d60bb95c5547c098fb797cc"
+    },
+    {
+        "entity_id": "5d60bb956248060626e14255"
+    },
+    {
+        "entity_id": "5d60bb95628a1644310800f9"
+    },
+    {
+        "entity_id": "5d60bb95349c09285c8efcd4"
+    },
+    {
+        "entity_id": "5d60bb9523814aebb651c3fd"
+    },
+    {
+        "entity_id": "5d60bb95ca90cd05eb6ce0f5"
+    },
+    {
+        "entity_id": "5d60bb95c6b41fc25aca42f8"
+    },
+    {
+        "entity_id": "5d60bb952d8816df6175da23"
+    },
+    {
+        "entity_id": "5d60bb95d7f1be79af93ad22"
+    },
+    {
+        "entity_id": "5d60bb95945a25e931e57df5"
+    },
+    {
+        "entity_id": "5d60bb951b4e76ead84abb00"
+    },
+    {
+        "entity_id": "5d60bb95e8ebec50ee4669d0"
+    },
+    {
+        "entity_id": "5d60bb959a45f1adaccb03c0"
+    },
+    {
+        "entity_id": "5d60bb95155bb72df944f168"
+    },
+    {
+        "entity_id": "5d60bb95d031c1214e2d3e13"
+    },
+    {
+        "entity_id": "5d60bb95485066bb23955bda"
+    },
+    {
+        "entity_id": "5d60bb953f0e3950701e8777"
+    },
+    {
+        "entity_id": "5d60bb957adba92b2e1a1fee"
+    },
+    {
+        "entity_id": "5d60bb95be29cdd89c51e877"
+    },
+    {
+        "entity_id": "5d60bb95dcd327ed918c21a3"
+    },
+    {
+        "entity_id": "5d60bb954e586744b6193396"
+    },
+    {
+        "entity_id": "5d60bb95318dff1de70e3a9e"
+    },
+    {
+        "entity_id": "5d60bb956c406788ff708458"
+    },
+    {
+        "entity_id": "5d60bb9570c3da05d01b0e04"
+    },
+    {
+        "entity_id": "5d60bb95357f4921f664ee82"
+    },
+    {
+        "entity_id": "5d60bb95f42676e747a4069f"
+    },
+    {
+        "entity_id": "5d60bb95f7c19158a0c9c803"
+    },
+    {
+        "entity_id": "5d60bb9552879908ac5df27e"
+    },
+    {
+        "entity_id": "5d60bb9577d3f0fc2790348d"
+    },
+    {
+        "entity_id": "5d60bb95f5f7bac8879a0765"
+    },
+    {
+        "entity_id": "5d60bb953f1cecfa6847cbb5"
+    },
+    {
+        "entity_id": "5d60bb9563658fa00cd9e9d3"
+    },
+    {
+        "entity_id": "5d60bb95b797c9abc8e23639"
+    },
+    {
+        "entity_id": "5d60bb95ad90378ec2f00229"
+    },
+    {
+        "entity_id": "5d60bb957f7e294a2eda178d"
+    },
+    {
+        "entity_id": "5d60bb95cf52ef6b19e65df8"
+    },
+    {
+        "entity_id": "5d60bb95fb65bbedbe6f6658"
+    },
+    {
+        "entity_id": "5d60bb957a50f39928ecfeb6"
+    },
+    {
+        "entity_id": "5d60bb953bc5abf6f9739b75"
+    },
+    {
+        "entity_id": "5d60bb95c8f9115505b46ea7"
+    },
+    {
+        "entity_id": "5d60bb9540379055b06dd472"
+    },
+    {
+        "entity_id": "5d60bb95561b36015d19e629"
+    },
+    {
+        "entity_id": "5d60bb957c994281b88a96c0"
+    },
+    {
+        "entity_id": "5d60bb955ff40c5f1c2bde1e"
+    },
+    {
+        "entity_id": "5d60bb956778341a1d48451b"
+    },
+    {
+        "entity_id": "5d60bb95ea5837cf39965d16"
+    },
+    {
+        "entity_id": "5d60bb9561e6ca498ba2f460"
+    },
+    {
+        "entity_id": "5d60bb953267b3c2eb11e7c0"
+    },
+    {
+        "entity_id": "5d60bb95108f6bfe2f387840"
+    },
+    {
+        "entity_id": "5d60bb9557bba9dac392e5d2"
+    },
+    {
+        "entity_id": "5d60bb9507ea3d40df2dd84e"
+    },
+    {
+        "entity_id": "5d60bb951a29cc5c8fe1c3b4"
+    },
+    {
+        "entity_id": "5d60bb956c6745cff5081c37"
+    },
+    {
+        "entity_id": "5d60bb959c2baf5d58ecc8ae"
+    },
+    {
+        "entity_id": "5d60bb95666e7eea1286e0bd"
+    },
+    {
+        "entity_id": "5d60bb95fd12c475af1b3dc5"
+    },
+    {
+        "entity_id": "5d60bb95542409b693d25a2d"
+    },
+    {
+        "entity_id": "5d60bb952e08475689322525"
+    },
+    {
+        "entity_id": "5d60bb956cb6dae394f11553"
+    },
+    {
+        "entity_id": "5d60bb95736cced7288a0e8c"
+    },
+    {
+        "entity_id": "5d60bb95529cec1d295028e9"
+    },
+    {
+        "entity_id": "5d60bb95e7924d455464bd36"
+    },
+    {
+        "entity_id": "5d60bb953e2de210b54ed582"
+    },
+    {
+        "entity_id": "5d60bb953a4a80fe1f4d35c6"
+    },
+    {
+        "entity_id": "5d60bb957491a893ccc9b074"
+    },
+    {
+        "entity_id": "5d60bb95c96a6463eaa55a96"
+    },
+    {
+        "entity_id": "5d60bb95d388ee80765e0a82"
+    },
+    {
+        "entity_id": "5d60bb95c90bb9fa26bb4e80"
+    },
+    {
+        "entity_id": "5d60bb95dfaf5d77b2e27702"
+    },
+    {
+        "entity_id": "5d60bb95fa63ed723cbb468d"
+    }
+];
+
+var AWS = require('aws-sdk');
+//Update with AWS config with your Secret ID and Secret Access Key
+var dynamodb = new AWS.DynamoDB();
+
+console.log(data.length);
+/**
+ *  function parameters are 
+ *  bulkInsert(
+ *              table_name, 
+ *              Array(normal array of objects), 
+ *              0, 
+ *              Array.length, 
+ *              true, 
+ *              DynamoDb Instance, 
+ *              callback Function
+ *              )
+ */
+bulkInsert("dummy_entity", data, 0, 25, true, dynamodb, function (err, response) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("All records inserted Successfully");
+    }
+});
+
+function bulkInsert(table__name, arr, start, end, flag, dynamodb, callback) {
+    if (flag) {
+        start = 0;
+        if (arr.length >= 25) {
+            end = 25;
+        } else {
+            end = arr.length;
+        }
+    }
+    let temp = {   //formated to object to insert into the params object
+        PutRequest: {
+            Item: {}
+        }
+    };
+    let params = {
+        RequestItems: {
+            [table__name]: []
+        }
+    };
+    for (i = start; i < end; i++) {
+        temp.PutRequest.Item = {};
+        let marsh = AWS.DynamoDB.Converter.marshall(arr[i]);
+        temp.PutRequest.Item = marsh;
+        params.RequestItems[table__name].push(JSON.parse(JSON.stringify(temp)));
+    }
+    dynamodb.batchWriteItem(params, function (err, data) {
+        if (err) {
+            console.log(err, err.stack);   // an error occurred
+            callback(err, null);
+        }
+        else {
+            if (arr.length > end) {
+                console.log(start, " ", end - 1, " ", "insertion Done");
+                start = end;
+                if (arr.length >= end + 25) {
+                    end = end + 25;
+                    bulkInsert(table__name, arr, start, end, false, dynamodb, callback);
+                } else {
+                    end = arr.length;
+                    bulkInsert(table__name, arr, start, end, false, dynamodb, callback);
+                }
+            } else {
+                console.log(start, " ", end - 1, " ", "insertion Done");
+                // console.log(":::::::::::::::", data);
+                callback(null, "success");           // successful response;
+            }
+
+        }
+    });
+
+}
